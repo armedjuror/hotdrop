39352f8bf4f67f51aaacc4d98b0059f2269a2446
Ajwad <ajwad@retainiq.io>
2026-05-22 01:45:19 +0530

    Footer Fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hotdrop-v1';
+const CACHE = 'hotdrop-v2';
 const STATIC = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', e => {
